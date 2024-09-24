06cd5cf4675e48a5ea3cf1233840f7c84995455b
Ojas Sarada <144619159+FastAndDanger2@users.noreply.github.com>
2024-09-23 19:53:27 -0500

    Add files via upload
    Password Generator in Python

---
+++ PasswordGenerator.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+def generatePassword(length, use_uppercase, use_digits, use_special_chars):
+    characters = string.ascii_lowercase 
+
+    if use_uppercase:
+        characters += string.ascii_uppercase
+    if use_digits:
+        characters += string.digits
+    if use_special_chars:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def password_generator_cli():
+    print("Simple Password Generator")
+
+    length = int(input("Enter password length: "))
+    use_uppercase = input("Include uppercase letters? (yes/no): ").lower() == 'yes'
+    use_digits = input("Include numbers? (yes/no): ").lower() == 'yes'
+    use_special_chars = input("Include special characters? (yes/no): ").lower() == 'yes'
+
+    password = generatePassword(length, use_uppercase, use_digits, use_special_chars)
+    print(f"Generated Password: {password}")
+
+if __name__ == "__main__":
+    password_generator_cli()
